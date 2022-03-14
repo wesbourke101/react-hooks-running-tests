2dc4a6ac96f93b48b5f1c13b8cd04fe0f4ea4f08
wesley <wesbourkeavl@gmail.com>
2022-03-14 10:07:41 -0400

    commit tree 1b3a5478044f496ac1bb7b9779d814817a45f715

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  test("displays the text 'please pass this test'", () => {
+    render(<Article />);
+  
+    // add this line
+    screen.debug();
+  
+    expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  });
 }
 
 export default Article;
